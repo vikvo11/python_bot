431832b11b9970b3309d369b80cf4476debdc5f5
vikvo11 <woroncull@yandex.ru>
2017-12-31 01:54:41 +0300

    add app.route

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from flask import Flask
 app = Flask(__name__)
 
 
-
+@app.route('/')
+def index():
+    return '<h1>Test flask app</h1>'
 
 if __name__ =='__main__':
     app.run(host='0.0.0.0',port = 5000)
