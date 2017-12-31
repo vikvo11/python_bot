de1526426a43cb7f9b65133403d60229596aac3c
vikvo11 <woroncull@yandex.ru>
2017-12-31 16:41:37 +0300

    s

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from misck import token,chat_id_old
 import requests
 import json
 
-#app = Flask(__name__)
+app = Flask(__name__)
 URL='https://api.telegram.org/bot{}/'.format(token)
 
 def write_json(data,filename='answer.json'):
@@ -23,22 +23,27 @@ def send_message(chatId,text='Please wait a few seconds...!'):
     print(answer)
     r=requests.get(url,json=answer)
     return r.json()
+
+    @app.route('/')
+        def index():
+            return'<h1>Hello Bot!</h1>'
 def main():
     #r=requests.get(URL+'getMe')
     #write_json(r.json())
     #print (r.json())
     #get_updates()
     #send_message(chat_id)
-    r = get_updates()
+    #r = get_updates()
     #print (r)
-    chat_id=r['result'][-1]['message']['chat']['id']
-    text=r['result'][-1]['message']['text']
-    print (str(chat_id)+' '+text)
-
+    #chat_id=r['result'][-1]['message']['chat']['id']
+    #text=r['result'][-1]['message']['text']
+    #print (str(chat_id)+' '+text)
+pass
 
 
 
 
 
 if __name__ =='__main__':
-    main()
+    #main()
+    app.run()
