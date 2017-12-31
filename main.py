ab6dbf01d4cd11291de2b63eced1dab48ebdf66b
vikvo11 <woroncull@yandex.ru>
2017-12-31 03:18:21 +0300

    tes

--- main.py
+++ main.py
@@ -3,10 +3,9 @@ from misck import token
 import requests
 #app = Flask(__name__)
 URL='https://api.telegram.org/bot{}/'.format(token)
-URLJenkins='http://admin:54e44e4e46a7929abb5f171d2ddc564c@35.177.112.94:8080/job/Send_msg/build --data-urlencode json='{"parameter": [{"name":"TOKEN", "value":"521265983:AAFUSq8QQzLUURwmCgXeBCjhRThRvf9YVM0"}, {"name":"CHAT_ID", "value":"488735610"}, {"name":"MSG", "value":"Sample\Plan"}]}''
 def main():
-    #r=requests.get(URL+'getMe')
-    r=requests.get(URLJenkins)
+    r=requests.get(URL+'getMe')
+    
     print (r.json())
 
 
