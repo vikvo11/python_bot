6caa78c7a9f46bba91ddfb8fb8285bc7d1aebb68
vikvo11 <woroncull@yandex.ru>
2017-12-31 03:26:23 +0300

    e

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from flask import Flask
 from misck import token
 import requests
+import json
+
 #app = Flask(__name__)
 URL='https://api.telegram.org/bot{}/'.format(token)
+
+def write_json(data,filename='answer.json'):
+    with open(filename,'w') as f:
+        json.dump(data,f,indent=2,ensure_ascii=False)
+
 def main():
     r=requests.get(URL+'getMe')
-    
+    write_json(r.json)
     print (r.json())
 
 
