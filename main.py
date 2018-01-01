3a22c33cdc198dc0ffd2104dafd033d52cb88cfc
vikvo11 <woroncull@yandex.ru>
2018-01-01 19:55:40 +0300

    s

--- main.py
+++ main.py
@@ -39,6 +39,10 @@ def index():
     if request.method=='POST':
         r = request.get_json()
         write_json(r)
+        chat_id=r['message']['chat']['id']
+        text=r['message']['text']
+        if 'bitcoin' in text:
+            send_message(chat_id,text+'- dorogoi'+str(update_id))
         return jsonify(r)
     return '<h1>Hello bot</h1>'
 def main():
