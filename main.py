0c9559325cb32ae4d74ede3d7ef6bfe92f1c6509
vikvo11 <woroncull@yandex.ru>
2018-01-03 02:27:49 +0300

    s

--- main.py
+++ main.py
@@ -94,6 +94,7 @@ class RegisterForm(Form):
     name = StringField('Name',[validators.length(min=1, max=50)])
     username = StringField('Username',[validators.length(min=4, max=25)])
     email = StringField('Email',[validators.length(min=6, max=50)])
+    token = StringField('Token',[validators.length(min=6, max=50)])
     password = PasswordField('Email',[
         validators.DataRequired(),
         validators.EqualTo('confirm', message='Password do not match')
