5ea8d5dee5d0e574bd7ec5d7926d4a224f4f2234
vikvo11 <woroncull@yandex.ru>
2018-01-02 03:59:08 +0300

    s

--- misck.py
+++ misck.py
@@ -4,12 +4,12 @@ chat_id_old='488735610'
 webhook='https://a5e19be0.ngrok.io/'
 
 
-if request.form['password'] == 'python' and request.form['username'] == 'vorovik':
-    session['logged_in'] = True
-    r='<h1>Session=True</h1>'
+#if request.form['password'] == 'python' and request.form['username'] == 'vorovik':
+#    session['logged_in'] = True
+#    r='<h1>Session=True</h1>'
     #get_password('vorovik')
-else:
-    flash('wrong password!')
-    return 'ok'
+#else:
+#    flash('wrong password!')
+#    return 'ok'
 #return home()
 #return '<h1>Session=True</h1>  '+r
