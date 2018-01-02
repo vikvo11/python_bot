ed4a7391d81a23547557e97fe71b0d8e30160529
vikvo11 <woroncull@yandex.ru>
2018-01-03 00:14:24 +0300

    s

--- main.py
+++ main.py
@@ -70,7 +70,9 @@ def get_pw(username):
 @app.route('/')
 def index():
     return render_template('home.html')
-
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 @app.route('/log')
 def home():
