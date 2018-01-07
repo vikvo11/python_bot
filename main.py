f8c5746d361d422f0781afecef218b8420d018ec
vikvo11 <woroncull@yandex.ru>
2018-01-07 19:52:35 +0300

    s

--- main.py
+++ main.py
@@ -255,7 +255,7 @@ def add_article():
         return redirect(url_for('dashbord'))
     return render_template('add_article.html',form=form)
 
-import views
+from views import do_admin_login
 
 @app.route('/login', methods=['POST','GET'])
 do_admin_login()
