c469259debd7ed0fa5b0758bf27b1c8076ec8050
vikvo11 <woroncull@yandex.ru>
2018-01-09 02:07:27 +0300

    q

--- app.py
+++ app.py
@@ -316,9 +316,7 @@ def webhook():
 
         global last_msg
         last_msg=json.dumps(r,ensure_ascii=False)
-		socketio.emit('my_response',
-                      {'data': text, 'chat_id': chat_id},
-                      namespace='/test')
+		socketio.emit('my_response',{'data': text, 'chat_id': chat_id},namespace='/test')
 
         return jsonify(r)
     return '<h1>Hello bot</h1>'
