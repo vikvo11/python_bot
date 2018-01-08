623b4932d7d8e67bd5836464ad3c1fd90161c522
vikvo11 <woroncull@yandex.ru>
2018-01-08 21:04:52 +0300

    s

--- app.py
+++ app.py
@@ -418,7 +418,7 @@ def test_disconnect():
 #-***************
 
 if __name__ =='__main__':
-    #socketio.run(app)
+    socketio.run(app)
     main()
     #app.run('0.0.0.0',port=5000)
     #socketio.run(app, debug=True)
