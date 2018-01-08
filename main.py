d8da909f74a174ad32801f5477eeb7c2e5257e91
vikvo11 <woroncull@yandex.ru>
2018-01-08 14:18:26 +0300

    s

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ import json # JSON modules
 import re # Regular expression - https://pythex.org/
 
 from version import base
-from flask_socketio import SocketIO, emit
+#from flask_socketio import SocketIO, emit
 
     #<Start -Declare> :
 global kk
@@ -315,31 +315,8 @@ def main():
     kk=0
     pass
 
-'''
-'''
-@app.route('/123')
-def index123():
-    return render_template('index.html')
-
-@socketio.on('my event', namespace='/test')
-def test_message(message):
-    emit('my response', {'data': message['data']})
-
-@socketio.on('my broadcast event', namespace='/test')
-def test_message(message):
-    emit('my response', {'data': message['data']}, broadcast=True)
-
-@socketio.on('connect', namespace='/test')
-def test_connect():
-    emit('my response', {'data': 'Connected'})
-
-@socketio.on('disconnect', namespace='/test')
-def test_disconnect():
-    print('Client disconnected')
-'''
-'''
 
 if __name__ =='__main__':
-    socketio.run(app)
+    #socketio.run(app)
     main()
     #app.run('0.0.0.0',port=5000)
