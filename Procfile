f52f18010f87c3a6508d9e102bce2ef216067753
vikvo11 <woroncull@yandex.ru>
2018-01-08 21:38:33 +0300

    s

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn -k gevent -w 1 app
+web: gunicorn app:app
--- app.py
+++ app.py
@@ -418,9 +418,9 @@ def test_disconnect():
 #-***************
 
 if __name__ =='__main__':
-    socketio.run(app)
+    #socketio.run(app)
     main()
     #app.run('0.0.0.0',port=5000)
     #socketio.run(app, debug=True)
 #socketio.run(app, host='0.0.0.0', port=5000, debug=True)
-    #app.run()      
+    #app.run()
