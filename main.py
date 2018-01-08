808dee3145b0bcf41a5720c2e253a1274e3190ed
vikvo11 <woroncull@yandex.ru>
2018-01-08 18:09:15 +0300

    s

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import re # Regular expression - https://pythex.org/
 
 from version import base
 #from flask_socketio import SocketIO, emit
- 
+
     #<Start -Declare> :
 global kk
 kk=0
@@ -320,3 +320,4 @@ if __name__ =='__main__':
     #socketio.run(app)
     main()
     #app.run('0.0.0.0',port=5000)
+    app.run()
