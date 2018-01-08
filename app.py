85ccfefc5b04871c72032299f06f6120bda63c35
vikvo11 <woroncull@yandex.ru>
2018-01-08 20:52:17 +0300

    s

--- app.py
+++ app.py
@@ -421,6 +421,6 @@ if __name__ =='__main__':
     #socketio.run(app)
     main()
     #app.run('0.0.0.0',port=5000)
-    socketio.run(app, debug=True)
+    #socketio.run(app, debug=True)
 #socketio.run(app, host='0.0.0.0', port=5000, debug=True)
     #app.run()
